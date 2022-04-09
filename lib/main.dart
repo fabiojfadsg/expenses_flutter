fd616eadaf76b5d574e8e27cec9329446d6cb283
Fabio Santos <fabio.santos@i-value.com.br>
2022-04-08 22:47:47 -0300

    Aula-97

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:expenses_flutter/models/transation.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
+
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -44,40 +45,46 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children: _transations.map((tr){
+            children: _transations.map((tr) {
               return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10,
-                      ),
-                      decoration: BoxDecoration(
+                  child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        )
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        tr.value.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Color.fromARGB(255, 175, 27, 201),
-                        ),
+                      color: Colors.black,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tr.value.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 175, 27, 201),
                       ),
                     ),
-                    Column(
-                      children: [
-                        Text(tr.title),
-                        Text(tr.date.toString()),
-                      ],
-                    )
-                  ],
-                )
-              );
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ));
             }).toList(),
           )
         ],
